29cf7fca8c024b7060e4cc987d2b6cc9ac7f3117
wei feng <theweifeng@gmail.com>
2019-04-13 01:49:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie][:known_for] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
