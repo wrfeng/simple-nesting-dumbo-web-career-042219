da9e163cbd8f70210472277e89250e3327184280
wei feng <theweifeng@gmail.com>
2019-04-13 01:49:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,13 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = 
+    {
+      known_for: "Ruby",
+      languages: ["LISP", "C"]
+    }
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -102,7 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
